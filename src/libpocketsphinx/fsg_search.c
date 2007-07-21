a8a72837a4192aa0770c57cfb2970c5e692647ab
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-07-21 11:50:47 +0000

    Fix segfault in dynamic FSG (patch from Nickolay V. Shmyrev)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@6941 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/libpocketsphinx/fsg_search.c
+++ src/libpocketsphinx/fsg_search.c
@@ -188,12 +188,10 @@ fsg_search_del_fsg(fsg_search_t * search, word_fsg_t * fsg)
         next = gnode_next(gn);
         if (oldfsg == fsg) {
             /* Found the FSG to be deleted; remove it from fsglist */
-            if (prev)
-                prev->next = next;
-            else
+            if (prev == NULL)
                 search->fsglist = next;
 
-            myfree((char *) gn, sizeof(gnode_t));
+            gnode_free (gn, prev);
 
             /* If this was the currently active FSG, also delete other stuff */
             if (search->fsg == fsg) {
