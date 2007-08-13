a7a0f66671bbe669cbb83d699eb015abdcf364d6
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-08-13 17:17:59 +0000

    Replace WSJ model with an 8kHz audio one trained on SI-284
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@7124 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ test/regression/test-wsj1-fwdtree.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+. ../testfuncs.sh
+
+bn=`basename $0 .sh`
+
+echo "Test: $bn"
+run_program pocketsphinx_batch \
+    -hmm $model/hmm/wsj1 \
+    -lm $data/wsj/wlist5o.nvp.lm.DMP \
+    -dict $data/wsj/wlist5o.nvp.dict \
+    -ctl $data/wsj/test5k.s1.ctl \
+    -cepdir $data/wsj \
+    -cepext .mfc \
+    -hyp $bn.match \
+    -fwdtree TRUE \
+    -fwdflat FALSE \
+    -bestpath FALSE \
+    > $bn.log 2>&1
+
+# Test whether it actually completed
+if [ $? = 0 ]; then
+    pass "run"
+else
+    fail "run"
+fi
+
+# Check the decoding results
+grep AVERAGE $bn.log
+$tests/word_align.pl -i $data/wsj/test5k.s1.lsn $bn.match | grep 'TOTAL Percent'
+compare_table "match" $data/wsj/$bn.match $bn.match
---
+++ test/regression/test-wsj1-n800-fwdtree.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+. ../testfuncs.sh
+
+bn=`basename $0 .sh`
+
+echo "Test: $bn"
+run_program pocketsphinx_batch \
+    -hmm $model/hmm/wsj1 \
+    -lm $data/wsj/wlist5o.nvp.lm.DMP \
+    -dict $data/wsj/wlist5o.nvp.dict \
+    -ctl $data/wsj/test5k.n800.ctl \
+    -cepdir $data/wsj \
+    -adcin yes -adchdr 44 -samprate 8000 \
+    -cepext .wav \
+    -hyp $bn.match \
+    -fwdtree TRUE \
+    -fwdflat FALSE \
+    -bestpath FALSE \
+    > $bn.log 2>&1
+
+# Test whether it actually completed
+if [ $? = 0 ]; then
+    pass "run"
+else
+    fail "run"
+fi
+
+# Check the decoding results
+grep AVERAGE $bn.log
+$tests/word_align.pl -i $data/wsj/test5k.n800.lsn $bn.match \
+    | grep 'TOTAL Percent'
+compare_table "match" $data/wsj/$bn.match $bn.match
