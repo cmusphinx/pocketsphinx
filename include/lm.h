da2b009c62be4cac793238e65afb2dff29f7877d
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-11-01 19:38:59 +0000

    Maintain lm_tg_score() and friends for source compatibility (oops)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@7367 94700074-3cef-4d97-a70e-9c8c206c02f5

--- include/lm.h
+++ include/lm.h
@@ -42,27 +42,11 @@
 #ifndef _LM_H_
 #define _LM_H_
 
+#include <lm_3g.h>
+
 /* LM scores, given sequences of dictionary base wid */
-int32	lm_tg_score (int32 w1, int32 w2, int32 w3);
-int32	lm_bg_score (int32 w1, int32 w2);
-int32	lm_ug_score (int32 w);
-
-/* One-time initialization of cache LM */
-void lm_cache_lm_init ( void );
-
-/*
- * Add a unigram (dictionary word id w) to cache LM (if doesn't exceed ugprob thresh).
- * The LM (and decoder) must be quiescent during this operation.
- */
-void lm_cache_lm_add_ug (int32 w);
-
-/*
- * Add a bigram (dictionary word id w1,w2) to cache LM.
- * The LM (and decoder) must be quiescent during this operation.
- */
-void lm_cache_lm_add_bg (int32 w1, int32 w2);
-
-void lm_cache_lm_dump (char *file);
-void lm_cache_lm_load (char *file);
+#define lm_tg_score(w1,w2,w3) lm3g_tg_score(w1,w2,w3)
+#define lm_bg_score(w1,w2) lm3g_bg_score(w1,w2)
+#define lm_ug_score(w) lm3g_ug_score(w)
 
 #endif
