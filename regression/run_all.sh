a04d5266f6a2bc1e2e01035ba4c1465e89c6b54d
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-01-08 05:32:45 +0000

    oops remove windowsisms
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@7554 94700074-3cef-4d97-a70e-9c8c206c02f5

--- regression/run_all.sh
+++ regression/run_all.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-platform=${1:-i686-winnt}
+platform=${1:-i386-linux}
 
 echo "SVN HEAD:"
-./wsj1_test5k.sh $platform-simple ../bin/Release/pocketsphinx_batch
-./wsj1_test5k_fast.sh $platform-fast ../bin/Release/pocketsphinx_batch
+./wsj1_test5k.sh $platform-simple ../$platform/src/programs/pocketsphinx_batch
+./wsj1_test5k_fast.sh $platform-fast ../$platform/src/programs/pocketsphinx_batch
 
-echo "0.3:"
-./wsj1_test5k.sh $platform-simple-base ../../pocketsphinx-0.3/bin/Release/pocketsphinx_batch
-./wsj1_test5k_fast.sh $platform-fast-base ../../pocketsphinx-0.3/bin/Release/pocketsphinx_batch
+echo "0.4.1:"
+./wsj1_test5k.sh $platform-simple-base ../../pocketsphinx-0.4.1/$platform/src/programs/pocketsphinx_batch
+./wsj1_test5k_fast.sh $platform-fast-base ../../pocketsphinx-0.4.1/$platform/src/programs/pocketsphinx_batch
