d37931fb22b84f3505b1d3221c9800bf45c2835f
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-01-17 21:49:44 +0000

    reflect the existence of the new stable branch
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@7590 94700074-3cef-4d97-a70e-9c8c206c02f5

--- regression/run_all.sh
+++ regression/run_all.sh
@@ -9,5 +9,5 @@ echo "SVN HEAD:"
 ./wsj1_test5k_fast.sh $platform-fast ../$platform/src/programs/pocketsphinx_batch
 
 echo "0.4.1:"
-./wsj1_test5k.sh $platform-simple-base ../../pocketsphinx-0.4.1/$platform/src/programs/pocketsphinx_batch
-./wsj1_test5k_fast.sh $platform-fast-base ../../pocketsphinx-0.4.1/$platform/src/programs/pocketsphinx_batch
+./wsj1_test5k.sh $platform-simple-base ../../pocketsphinx-0.4/pocketsphinx/$platform/src/programs/pocketsphinx_batch
+./wsj1_test5k_fast.sh $platform-fast-base ../../pocketsphinx-0.4/pocketsphinx/$platform/src/programs/pocketsphinx_batch
