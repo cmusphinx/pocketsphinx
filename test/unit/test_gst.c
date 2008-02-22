c7ce0ad6952cfa22466626bd0a0bf2d57d5b43b5
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-02-22 22:03:44 +0000

    Most simplest GStreamer test case I could think of
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@7854 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ test/unit/test_gst.c
@@ -0,0 +1,68 @@
+#include <gst/gst.h>
+
+static gboolean
+bus_call(GstBus *bus, GstMessage *msg, gpointer data)
+{
+	GMainLoop *loop = (GMainLoop *)data;
+
+	switch (GST_MESSAGE_TYPE(msg)) {
+	case GST_MESSAGE_EOS:
+		g_print("End-of-stream\n");
+		g_main_loop_quit(loop);
+		break;
+	case GST_MESSAGE_ERROR: {
+		gchar *debug;
+		GError *err;
+
+		gst_message_parse_error(msg, &err, &debug);
+		g_free(debug);
+		g_print("Error: %s\n", err->message);
+		g_error_free(err);
+		g_main_loop_quit(loop);
+		break;
+	}
+	default:
+		break;
+	}
+
+	return TRUE;
+}
+
+int
+main(int argc, char *argv[])
+{
+	GMainLoop *loop;
+	GstElement *pipeline;
+	GstElement *src, *resamp, *sink;
+	GstCaps *caps;
+	GstBus *bus;
+
+	gst_init(&argc, &argv);
+	loop = g_main_loop_new(NULL, FALSE);
+
+	pipeline = gst_pipeline_new("test_gst");
+	src = gst_element_factory_make("filesrc", "file-source");
+	g_object_set(G_OBJECT(src), "location", DATADIR "/goforward.raw", NULL);
+	caps = gst_caps_from_string("audio/x-raw-int,channels=1,endianness=1234,width=16,depth=16,rate=16000,signed=true");
+	if (caps == NULL) {
+		g_print("Failed to create caps!\n");
+		return 1;
+	}
+	resamp = gst_element_factory_make("audioresample", "resampler");
+	sink = gst_element_factory_make("pocketsphinx", "asr-sink");
+	gst_bin_add_many(GST_BIN(pipeline),
+			 src, resamp, sink, NULL);
+	gst_element_link_filtered(src, resamp, caps);
+	gst_element_link(resamp, sink);
+
+	bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline));
+	gst_bus_add_watch(bus, bus_call, loop);
+
+	gst_element_set_state(pipeline, GST_STATE_PLAYING);
+	g_main_loop_run(loop);
+
+	gst_element_set_state(pipeline, GST_STATE_NULL);
+	gst_object_unref(GST_OBJECT(pipeline));
+
+	return 0;
+}
