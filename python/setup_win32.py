9e5d53a433dac2d7178114af3458b870c72d0026
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-07-22 21:04:41 +0000

    Add a setup script for Windows
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@8626 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ python/setup_win32.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup, Extension
+except:
+    from distutils.core import setup, Extension
+
+import os
+
+sb_includes = ['../../sphinxbase/include', '../../sphinxbase/win32/include']
+sb_libdirs = ['../../sphinxbase/lib/debug']
+
+setup(name = 'PocketSphinx',
+      version = '0.5.1',
+      author = 'David Huggins-Daines',
+      author_email = 'dhuggins@cs.cmu.edu',
+      description = 'Python interface to CMU PocketSphinx speech recognition',
+      license = 'BSD',
+      url = 'http://cmusphinx.org',
+      ext_modules = [
+        Extension('pocketsphinx',
+                   sources=['pocketsphinx.c'],
+                   libraries=['pocketsphinx', 'sphinxbase'],
+                   include_dirs=['../include',
+                                 '../python'] + sb_includes,
+                   library_dirs=['../bin/debug'] + sb_libdirs)
+        ],
+     ) 
