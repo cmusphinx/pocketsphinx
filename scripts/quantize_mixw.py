cf4a16e388457154eaaad09c478ef2c4f2a055bb
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2010-01-12 03:47:08 +0000

    Add Huffman-coded sendump format - actually the code for this isn't quite right yet (for one thing the "zero" value ends up being redundant which hurts compression)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@9637 94700074-3cef-4d97-a70e-9c8c206c02f5

--- scripts/quantize_mixw.py
+++ scripts/quantize_mixw.py
@@ -7,6 +7,7 @@ import sys
 import prune_mixw as pm
 import struct
 import s3mixw
+import sphinxbase
 
 def mixw_kmeans_iter(mixw, cb):
     cbacc = numpy.zeros(len(cb))
@@ -104,6 +105,62 @@ def write_sendump_hb(mixwmap, cb, outfile):
             mm.tofile(fh)
     fh.close()
 
+fmtdesc4 = \
+"""BEGIN FILE FORMAT DESCRIPTION
+(int32) <length(string)> (including trailing 0)
+<string> (including trailing 0)
+... preceding 2 items repeated any number of times
+(int32) 0 (length(string)=0 terminates the header)
+codebook_count <codebook_count>
+mixture_count <mixture_count>
+model_count <model_count>
+cluster_count <cluster_count>
+huffman_coded 1
+logbase <logarithm_base>
+mixw_shift <log_bits_downshifted>
+<huffman codebook>
+<compressed arrays of mixture weights>
+END FILE FORMAT DESCRIPTION
+feature_count %d
+codebook_count 1
+mixture_count %d
+model_count %d
+cluster_count %d
+huffman_coded 1
+logbase 1.0001
+mixw_shift 10"""
+
+def write_sendump_huff(mixwmap, cb, outfile):
+    n_sen, n_feat, n_gau = mixwmap.shape
+    fh = open(outfile, "wb")
+    # Write the header
+    fmtdesc0 = fmtdesc3 % (n_feat, n_gau, n_sen, len(cb))
+    for line in fmtdesc0.split('\n'):
+        fh.write(struct.pack('>I', len(line) + 1))
+        fh.write(line)
+        fh.write('\0')
+    # Terminate it with a null entry
+    fh.write(struct.pack('>I', 0))
+    # Add one extra index to the end to hold the "zero" value
+    qcb = numpy.resize(-(cb / numpy.log(1.0001)).astype('i') >> 10, len(cb)+1)
+    qcb[-1] = 159
+    # Histogram the mixture weight map and build a Huffman codebook
+    hist = numpy.zeros(len(qcb), 'i')
+    for cw in mixwmap.ravel():
+        hist[cw] += 1
+    # Write the codebook (we code directly to quantized mixw values)
+    huff = sphinxbase.HuffCode(zip(qcb, hist))
+    huff.write(fh)
+    # Now Huffman code the mixture weights (not their codebook indices
+    # mind you!) to the output file.
+    huff.attach(fh, "wb")
+    for f in range(0, n_feat):
+        for g in range(0, n_gau):
+            syms = [qcb[x] for x in mixwmap[:,f,g]]
+            huff.encode_to_file(syms)
+    huff.detach()
+    fh.close()
+
 def read_sendump(infile):
     def readstr(fh):
         nbytes = struct.unpack('I', fh.read(4))[0]
@@ -137,5 +194,5 @@ if __name__ == '__main__':
     ifn, ofn = sys.argv[1:]
     mixw = read_sendump(ifn)
     cb = quantize_mixw_kmeans(mixw, 15, 1.5e-7)
-    mwmap = map_mixw_cb(mixw, cb, 1e.5-7)
+    mwmap = map_mixw_cb(mixw, cb, 1.5e-7)
     pm.write_sendump_hb(mwmap, mixw, ofn)
