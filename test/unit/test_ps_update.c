d42cf50e02ab81de830d8f792618b92d3333293c
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2011-04-14 18:39:43 +0000

    Fixes distcheck
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@10840 94700074-3cef-4d97-a70e-9c8c206c02f5

--- test/unit/test_ps_update.c
+++ test/unit/test_ps_update.c
@@ -5,6 +5,9 @@ static void test_reinit_fsg()
        ps_decoder_t *ps;
        cmd_ln_t *config;
        config = cmd_ln_init(NULL, ps_args(), TRUE,
+			    "-hmm", MODELDIR "/hmm/en_US/hub4wsj_sc_8k",
+			    "-lm", MODELDIR "/lm/en_US/wsj0vp.5000.DMP",
+			    "-dict", MODELDIR "/lm/en_US/cmu07a.dic",
                             NULL);
        ps = ps_init(config);
        ps_update_fsgset (ps);
@@ -17,6 +20,9 @@ static void test_reinit_lm()
         cmd_ln_t *config;
         ngram_model_t *model;
         config = cmd_ln_init(NULL, ps_args(), TRUE,
+    			     "-hmm", MODELDIR "/hmm/en_US/hub4wsj_sc_8k",
+			     "-lm", MODELDIR "/lm/en_US/wsj0vp.5000.DMP",
+			     "-dict", MODELDIR "/lm/en_US/cmu07a.dic",
                              NULL);
         ps = ps_init(config);
         model = ps_update_lmset (ps, NULL);
