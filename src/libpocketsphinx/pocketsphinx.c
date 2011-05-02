6d627679cbcafd47764b6610f17cd753fb45c819
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2011-05-02 19:35:44 +0000

    Log is already configured in sphinxbase, no need to do it here.
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@10957 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/libpocketsphinx/pocketsphinx.c
+++ src/libpocketsphinx/pocketsphinx.c
@@ -198,11 +198,7 @@ ps_reinit(ps_decoder_t *ps, cmd_ln_t *config)
         cmd_ln_free_r(ps->config);
         ps->config = config;
     }
-#ifndef _WIN32_WCE
-    /* Set up logging. */
-    if (cmd_ln_str_r(ps->config, "-logfn"))
-        err_set_logfile(cmd_ln_str_r(ps->config, "-logfn"));
-#endif
+
     err_set_debug_level(cmd_ln_int32_r(ps->config, "-debug"));
     ps->mfclogdir = cmd_ln_str_r(ps->config, "-mfclogdir");
     ps->rawlogdir = cmd_ln_str_r(ps->config, "-rawlogdir");
