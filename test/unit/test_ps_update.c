8f6c8ccf9bb8d3fb24ea6f5003c1f9af835e8d93
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2012-10-30 10:20:41 +0000

    Added a test for ps_update_lmset with NULL as lmset to work properly
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@11641 94700074-3cef-4d97-a70e-9c8c206c02f5

--- test/unit/test_ps_update.c
+++ test/unit/test_ps_update.c
@@ -1,4 +1,5 @@
 #include <pocketsphinx.h>
+#include "test_macros.h"
 
 static void test_reinit_fsg()
 {
@@ -47,10 +48,36 @@ static void test_reinit_lm()
         cmd_ln_free_r(config);
 }
 
+/**
+ * Test for NULL in ps_update_lmset return the existing lmset as
+ * specified in API reference
+ */
+static void test_switch_fsg_lm()
+{
+        ps_decoder_t *ps;
+        cmd_ln_t *config;
+        ngram_model_t *model;
+        config = cmd_ln_init(NULL, ps_args(), TRUE,
+    			     "-hmm", MODELDIR "/hmm/en_US/hub4wsj_sc_8k",
+			     "-lm", MODELDIR "/lm/en_US/wsj0vp.5000.DMP",
+			     "-dict", MODELDIR "/lm/en_US/cmu07a.dic",
+                             NULL);
+        ps = ps_init(config);
+        ps_update_fsgset (ps);
+        TEST_ASSERT(ps_get_lmset(ps) == NULL);
+        model = ps_update_lmset (ps, NULL);
+        TEST_ASSERT(model != NULL);
+        TEST_ASSERT(ps_get_lmset(ps) != NULL);
+        ps_free(ps);
+        cmd_ln_free_r(config);
+}
+
+
 int main(int argc, char* argv[]) 
 {
 	test_reinit_fsg();
 	test_reinit_fsg_missing();
 	test_reinit_lm();
+	test_switch_fsg_lm();
 	return 0;
 }
