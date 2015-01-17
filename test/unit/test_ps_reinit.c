2a3d71074bd813d81d68d4a61423cf084559eca2
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-01-17 17:48:38 +0000

    Fixes crash on ps_add_word
    See for details https://sourceforge.net/p/cmusphinx/bugs/411/
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@12792 94700074-3cef-4d97-a70e-9c8c206c02f5

--- test/unit/test_ps_reinit.c
+++ test/unit/test_ps_reinit.c
@@ -12,28 +12,22 @@ main(int argc, char *argv[])
 
 	TEST_ASSERT(config =
 		    cmd_ln_init(NULL, ps_args(), TRUE,
-				"-hmm", MODELDIR "/en-us/en-us",
-				"-lm", MODELDIR "/en-us/en-us.lm.dmp",
-				"-dict", MODELDIR "/en-us/cmudict-en-us.dict",
-				"-fwdtree", "yes",
-				"-fwdflat", "yes",
-				"-bestpath", "yes",
-				"-input_endian", "little",
-				"-samprate", "16000", NULL));
+				"-hmm", DATADIR "/tidigits/hmm",
+				"-dict", DATADIR "/tidigits/lm/tidigits.dic",
+				NULL));
 	TEST_ASSERT(ps = ps_init(config));
 	cmd_ln_free_r(config);
 
 	TEST_ASSERT(config =
 		    cmd_ln_init(NULL, ps_args(), TRUE,
-				"-hmm", DATADIR "/tidigits/hmm",
-				"-lm", DATADIR "/tidigits/lm/tidigits.lm.dmp",
-				"-dict", DATADIR "/tidigits/lm/tidigits.dic",
-				"-fwdtree", "yes",
-				"-fwdflat", "yes",
-				"-bestpath", "yes",
-				"-input_endian", "little",
-				"-samprate", "16000", NULL));
+				"-hmm", MODELDIR "/en-us/en-us",
+				"-dict", MODELDIR "/en-us/cmudict-en-us.dict",
+				NULL));
 	TEST_EQUAL(0, ps_reinit(ps, config));
+
+	/* Reinit when adding words */
+	ps_add_word(ps, "foobie", "F UW B IY", FALSE);
+	ps_add_word(ps, "hellosomething", "HH EH L OW S", TRUE);
 
 	ps_free(ps);
 	cmd_ln_free_r(config);
