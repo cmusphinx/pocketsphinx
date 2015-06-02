5331ca83f99e3ab3169c383b096074f86e51601c
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-06-02 21:05:03 +0000

    Fixes documentation.
    Thanks to Sean Robertson.
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@13016 94700074-3cef-4d97-a70e-9c8c206c02f5

--- include/pocketsphinx.h
+++ include/pocketsphinx.h
@@ -88,8 +88,8 @@ ps_default_search_args(cmd_ln_t *);
  * Initialize the decoder from a configuration object.
  *
  * @note The decoder retains ownership of the pointer
- * <code>config</code>, so you must not attempt to free it manually.
- * If you wish to reuse it elsewhere, call cmd_ln_retain() on it.
+ * <code>config</code>, so if you are not going to use it
+ * elsewere, you can free it.
  *
  * @param config a command-line structure, as created by
  * cmd_ln_parse_r() or cmd_ln_parse_file_r().
