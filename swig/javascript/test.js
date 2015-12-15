bc4b72a6993c63291c9a1f71e1cc49ad655f404d
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2015-12-15 08:06:03 +0000

    Add Javascript example
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx@13137 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ swig/javascript/test.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+var ps = require('.').ps;
+
+modeldir = "../../model/en-us/"
+
+var config = new ps.Decoder.defaultConfig();
+config.setString("-hmm", modeldir + "en-us");
+config.setString("-dict", modeldir + "cmudict-en-us.dict");
+config.setString("-lm", modeldir + "en-us.lm.bin");
+var decoder = new ps.Decoder(config);
+
+fs.readFile("../../test/data/goforward.raw", function(err, data) {
+    if (err) throw err;
+    decoder.startUtt();
+    decoder.processRaw(data, false, false);
+    decoder.endUtt();
+    console.log(decoder.hyp())
+
+    it = decoder.seg().iter()
+    while ((seg = it.next()) != null) {
+        console.log(seg.word, seg.startFrame, seg.endFrame);
+    }
+});
+
