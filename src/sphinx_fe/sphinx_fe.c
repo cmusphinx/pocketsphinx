61f984ffdf4281a01a9e62377b9bd10c9914dd3f
David Huggins-Daines <dhdaines@gmail.com>
2022-06-07 21:27:08 -0400

    include cmd_ln in sphinx_fe

--- src/sphinx_fe/sphinx_fe.c
+++ src/sphinx_fe/sphinx_fe.c
@@ -55,7 +55,173 @@
 #include <sphinxbase/hash_table.h>
 
 #include "sphinx_wave2feat.h"
-#include "cmd_ln_defn.h"
+
+const char helpstr[] =
+  "Description: \n\
+Extract acoustic features form from audio file.\n\
+\n\
+The main parameters that affect the final output, with typical values, are:\n\
+\n\
+samprate, typically 8000, 11025, or 16000\n\
+lowerf, 130, 200, 130, for the respective sampling rates above\n\
+upperf, 3700, 5200, 6800, for the respective sampling rates above\n\
+nfilt, 31, 37, 40, for the respective sampling rates above\n\
+nfft, 256 or 512\n\
+format, raw or nist or mswav\n\
+\"";
+
+const char examplestr[] =
+  "Example: \n\
+This example creates a cepstral file named \"output.mfc\" from an input audio file named \"input.raw\", which is a raw audio file (no header information), which was originally sampled at 16kHz. \n\
+\n\
+sphinx_fe -i  input.raw \n\
+        -o   output.mfc \n\
+        -input_endian little \n\
+        -samprate  16000 \n\
+        -lowerf    130 \n\
+        -upperf    6800 \n\
+        -nfilt     40 \n\
+        -nfft      512";
+
+static arg_t defn[] = {
+  { "-help",
+    ARG_BOOLEAN,
+    "no",
+    "Shows the usage of the tool"},
+  
+  { "-example",
+    ARG_BOOLEAN,
+    "no",
+    "Shows example of how to use the tool"},
+
+  waveform_to_cepstral_command_line_macro(),
+
+  { "-argfile",
+    ARG_STRING,
+    NULL,
+    "Argument file (e.g. feat.params from an acoustic model) to read parameters from.  This will override anything set in other command line arguments." },
+  
+  { "-i",
+    ARG_STRING,
+    NULL,
+    "Single audio input file" },
+  
+  { "-o",
+    ARG_STRING,
+    NULL,
+    "Single cepstral output file" },
+  
+  { "-c",
+    ARG_STRING,
+    NULL,
+    "Control file for batch processing" },
+  
+  { "-nskip",
+    ARG_INT32,
+    "0",
+    "If a control file was specified, the number of utterances to skip at the head of the file" },
+  
+  { "-runlen",
+    ARG_INT32,
+    "-1",
+    "If a control file was specified, the number of utterances to process, or -1 for all" },
+
+  { "-part",
+    ARG_INT32,
+    "0",
+    "Index of the part to run (supersedes -nskip and -runlen if non-zero)" },
+  
+  { "-npart",
+    ARG_INT32,
+    "0",
+    "Number of parts to run in (supersedes -nskip and -runlen if non-zero)" },
+  
+  { "-di",
+    ARG_STRING,
+    NULL,
+    "Input directory, input file names are relative to this, if defined" },
+  
+  { "-ei",
+    ARG_STRING,
+    NULL,
+    "Input extension to be applied to all input files" },
+  
+  { "-do",
+    ARG_STRING,
+    NULL,
+    "Output directory, output files are relative to this" },
+  
+  { "-eo",
+    ARG_STRING,
+    NULL,
+    "Output extension to be applied to all output files" },
+  
+  { "-build_outdirs",
+    ARG_BOOLEAN,
+    "yes",
+    "Create missing subdirectories in output directory" },
+
+  { "-sph2pipe",
+    ARG_BOOLEAN,
+    "no",
+    "Input is NIST sphere (possibly with Shorten), use sph2pipe to convert" },
+
+  { "-nist",
+    ARG_BOOLEAN,
+    "no",
+    "Defines input format as NIST sphere" },
+  
+  { "-raw",
+    ARG_BOOLEAN,
+    "no",
+    "Defines input format as raw binary data" },
+  
+  { "-mswav",
+    ARG_BOOLEAN,
+    "no",
+    "Defines input format as Microsoft Wav (RIFF)" },
+  
+  { "-nchans",
+    ARG_INT32,
+    "1",
+    "Number of channels of data (interlaced samples assumed)" },
+  
+  { "-whichchan",
+    ARG_INT32,
+    "0",
+    "Channel to process (numbered from 1), or 0 to mix all channels" },
+  
+  { "-ofmt",
+    ARG_STRING,
+    "sphinx",
+    "Format of output files - one of sphinx, htk, text." },
+  
+  { "-mach_endian",
+    ARG_STRING,
+#ifdef WORDS_BIGENDIAN
+    "big",
+#else
+    "little",
+#endif
+    "Endianness of machine, big or little" },
+  
+  { "-blocksize",
+    ARG_INT32,
+    "2048",
+    "Number of samples to read at a time." },
+
+  { "-spec2cep",
+    ARG_BOOLEAN,
+    "no",
+    "Input is log spectral files, output is cepstral files" },
+
+  { "-cep2spec",
+    ARG_BOOLEAN,
+    "no",
+    "Input is cepstral files, output is log spectral files" },
+
+  { NULL, 0, NULL, NULL }
+};
 
 typedef struct audio_type_s {
     char const *name;
