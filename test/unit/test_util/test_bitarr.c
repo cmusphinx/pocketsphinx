3f124110214cd69009dc211f05e560056db7a7b1
David Huggins-Daines <dhdaines@gmail.com>
2022-06-09 18:39:57 +0000

    fix: test more (but not enough, evidently) things

--- test/unit/test_util/test_bitarr.c
+++ test/unit/test_util/test_bitarr.c
@@ -25,7 +25,7 @@ main(int argc, char *argv[])
     bitarr_address_t address;
 
     err_set_loglevel(ERR_INFO);
-    //sign bit should be 0x80000000
+    //sign bit should be 0x80000000 (BUT WHY?!?!?)
     TEST_EQUAL((neg1.i ^ pos1.i), 0x80000000);
     memset(mem, 0, sizeof(mem));
     address.base = mem;
@@ -46,12 +46,13 @@ main(int argc, char *argv[])
     for (address.offset = 0; address.offset < 57 * 8; address.offset += 1) {
       uint64 read57;
 
+      memset(mem, 0, sizeof(mem));
       bitarr_write_int57(address, 57, test57);
       read57 = bitarr_read_int57(address, 57, ((1ULL << 57) - 1));
       E_INFO("Testing %llx at %d = %llx\n", test57, address.offset, read57);
       TEST_EQUAL(test57, read57);
-      memset(mem, 0, sizeof(mem));
     }
+    memset(mem, 0, sizeof(mem));
 
     /* Test packed bits */
     for (address.offset = 0; address.offset < 57 * 8; address.offset += 25)
@@ -61,19 +62,36 @@ main(int argc, char *argv[])
     E_INFO("%d\n", mem[2]);
     E_INFO("%d\n", mem[3]);
     for (address.offset = 0; address.offset < 57 * 8; address.offset += 25) {
-      uint64 read25 = bitarr_read_int25(address, 25, ((1UL << 25) - 1));
+      uint32 read25 = bitarr_read_int25(address, 25, ((1UL << 25) - 1));
       E_INFO("Testing %lx at %d = %lx\n", test25, address.offset, read25);
       TEST_EQUAL(test25, read25);
     }
     /* Test arbitrary addresses */
     for (address.offset = 0; address.offset < 57 * 8; address.offset += 1) {
-      uint64 read25;
+      uint32 read25;
 
+      memset(mem, 0, sizeof(mem));
       bitarr_write_int25(address, 25, test25);
       read25 = bitarr_read_int25(address, 25, ((1UL << 25) - 1));
       E_INFO("Testing %lx at %d = %lx\n", test25, address.offset, read25);
       TEST_EQUAL(test25, read25);
-      memset(mem, 0, sizeof(mem));
+    }
+    memset(mem, 0, sizeof(mem));
+
+    /* Test mixing 32 and 64-bit access. */
+    for (address.offset = 0; address.offset < 57 * 8; address.offset += 82) {
+      bitarr_address_t address2 = address;
+      uint64 read25;
+
+      bitarr_write_int25(address, 25, test25);
+      address2.offset += 25;
+      bitarr_write_int57(address2, 57, test57);
+      read25 = bitarr_read_int25(address, 25, ((1UL << 25) - 1));
+      E_INFO("Testing %lx at %d = %lx\n", test25, address.offset, read25);
+      TEST_EQUAL(test25, read25);
+      read25 = bitarr_read_int57(address2, 57, ((1ULL << 57) - 1));
+      E_INFO("Testing %llx at %d = %llx\n", test57, address2.offset, read25);
+      TEST_EQUAL(test57, read25);
     }
     return 0;
 }
