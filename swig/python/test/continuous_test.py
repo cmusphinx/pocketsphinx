710722890d83df946397395529b9422041cb4587
David Huggins-Daines <dhdaines@gmail.com>
2022-07-05 17:51:19 -0400

    test: put arbitrary CMN init in continuous test

--- swig/python/test/continuous_test.py
+++ swig/python/test/continuous_test.py
@@ -15,6 +15,7 @@ class TestContinuous(unittest.TestCase):
         config.set_string('-hmm', os.path.join(MODELDIR, 'en-us/en-us'))
         config.set_string('-lm', os.path.join(MODELDIR, 'en-us/en-us.lm.bin'))
         config.set_string('-dict', os.path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+        config.set_string('-cmninit', '41.00,-5.29,-0.12,5.09,2.48,-4.07,-1.37,-1.78,-5.08,-2.05,-6.45,-1.42,1.17')
         decoder = Decoder(config)
 
         stream = open(os.path.join(DATADIR, 'goforward.raw'), 'rb')
