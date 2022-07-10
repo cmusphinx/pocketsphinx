97cecf23d7f793e44c0a1f6f19f399763833bb79
David Huggins-Daines <dhdaines@gmail.com>
2022-07-10 10:18:56 -0400

    build: add build_wheels.sh (but it's broken for recent Docker images)

--- build_wheels.sh
+++ build_wheels.sh
@@ -11,7 +11,9 @@ many1_run() {
 many2014_run() {
     docker run -v $PWD:$PWD -v $HOME/.cache/pip:/.cache/pip -u $U:$G -w $PWD -it quay.io/pypa/manylinux2014_x86_64 "$@"
 }
-	       
+many224_run() {
+    docker run -v $PWD:$PWD -v $HOME/.cache/pip:/.cache/pip -u $U:$G -w $PWD -it quay.io/pypa/manylinux_2_24_x86_64 "$@"
+}
 
 python setup.py clean || true
 rm -rf *.whl dist/* py/pocketsphinx.egg-info
@@ -20,9 +22,10 @@ docker pull quay.io/pypa/manylinux1_x86_64
 for version in cp39-cp39 cp38-cp38 cp37-cp37m; do
     many1_run /opt/python/$version/bin/pip wheel dist/pocketsphinx5-$VERSION.tar.gz
 done
-docker pull quay.io/pypa/manylinux2014_x86_64
-many2014_run /opt/python/cp310-cp310/bin/pip wheel dist/pocketsphinx5-$VERSION.tar.gz
+# Currently BROKEN: see https://github.com/pypa/manylinux/issues/1347
+# docker pull quay.io/pypa/manylinux2014_x86_64
+# many2014_run /opt/python/cp310-cp310/bin/pip wheel dist/pocketsphinx5-$VERSION.tar.gz
 for w in *.whl; do
-    many2014_run auditwheel repair -w dist $w
+    many1_run auditwheel repair -w dist $w
     rm $w
 done
