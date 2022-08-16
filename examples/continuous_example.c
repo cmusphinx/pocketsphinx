4d71db9638bc8ea23466d0b4b787399cdeb14820
David Huggins-Daines <dhdaines@gmail.com>
2022-08-16 09:41:58 -0400

    feat: add simple examples

---
+++ examples/continuous_example.c
@@ -0,0 +1,55 @@
+#include <pocketsphinx.h>
+#include <signal.h>
+
+static int global_done = 0;
+void
+catch_sig(int signum)
+{
+    global_done = 1;
+}
+
+int
+main(int argc, char *argv[])
+{
+    ps_decoder_t *decoder;
+    cmd_ln_t *config;
+    char *soxcmd;
+    FILE *sox;
+    #define BUFLEN 1024
+    short buf[BUFLEN];
+    size_t len;
+
+    if ((config = cmd_ln_parse_r(NULL, ps_args(),
+                                 argc, argv, TRUE)) == NULL)
+        E_FATAL("Command line parse failed\n");
+    ps_default_search_args(config);
+    if ((decoder = ps_init(config)) == NULL)
+        E_FATAL("PocketSphinx decoder init failed\n");
+    #define SOXCMD "sox -q -r %d -c 1 -b 16 -e signed-integer -d -t raw -"
+    len = snprintf(NULL, 0, SOXCMD,
+                   (int)cmd_ln_float_r(config, "-samprate"));
+    soxcmd = malloc(len + 1);
+    if (signal(SIGINT, catch_sig) == SIG_ERR)
+        E_FATAL_SYSTEM("Failed to set SIGINT handler");
+    if (snprintf(soxcmd, len + 1, SOXCMD,
+                 (int)cmd_ln_float_r(config, "-samprate")) != len)
+        E_FATAL_SYSTEM("snprintf() failed");
+    if ((sox = popen(soxcmd, "r")) == NULL)
+        E_FATAL_SYSTEM("Failed to popen(%s)", soxcmd);
+    free(soxcmd);
+    ps_start_utt(decoder);
+    while (!global_done) {
+        len = fread(buf, sizeof(buf[0]), BUFLEN, sox);
+        if (ps_process_raw(decoder, buf, len, FALSE, FALSE) < 0)
+            E_FATAL("ps_process_raw() failed\n");
+    }
+    ps_end_utt(decoder);
+    if (pclose(sox) < 0)
+        E_ERROR_SYSTEM("Failed to pclose(sox)");
+    if (ps_get_hyp(decoder, NULL) != NULL)
+        printf("%s\n", ps_get_hyp(decoder, NULL));
+    cmd_ln_free_r(config);
+    ps_free(decoder);
+        
+    return 0;
+}
---
+++ examples/continuous_example.py
@@ -0,0 +1,26 @@
+from pocketsphinx5 import Decoder
+import subprocess
+import os
+MODELDIR = os.path.join(os.path.dirname(__file__), os.path.pardir, "model")
+BUFSIZE = 1024
+
+decoder = Decoder(
+    hmm=os.path.join(MODELDIR, "en-us/en-us"),
+    lm=os.path.join(MODELDIR, "en-us/en-us.lm.bin"),
+    dict=os.path.join(MODELDIR, "en-us/cmudict-en-us.dict"),
+)
+sample_rate = int(decoder.config["samprate"])
+soxcmd = f"sox -q -r {sample_rate} -c 1 -b 16 -e signed-integer -d -t raw -"
+with subprocess.Popen(soxcmd.split(), stdout=subprocess.PIPE) as sox:
+    decoder.start_utt()
+    try:
+        while True:
+            buf = sox.stdout.read(BUFSIZE)
+            if len(buf) == 0:
+                break
+            decoder.process_raw(buf)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        decoder.end_utt()
+    print(decoder.hyp().hypstr)
