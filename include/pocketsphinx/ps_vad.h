114723ed64268800ece7649e56e68f51a619c944
David Huggins-Daines <dhdaines@gmail.com>
2022-08-19 18:24:32 -0400

    docs: slim down copyright notice
    It's not clear that I could actually assign copyright to CMU,
    so I won't do that

--- include/pocketsphinx/ps_vad.h
+++ include/pocketsphinx/ps_vad.h
@@ -1,7 +1,6 @@
 /* -*- c-basic-offset:4; indent-tabs-mode: nil -*- */
 /* ====================================================================
- * Copyright (c) 2022 Carnegie Mellon University.  All rights
- * reserved.
+ * Copyright (c) 2022 David Huggins-Daines.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,22 +13,7 @@
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- *
- *
- * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
- * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
- * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  * ====================================================================
- *
  */
 /**
  * @file ps_vad.h Voice activity detection for PocketSphinx.
