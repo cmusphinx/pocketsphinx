bf4518605526685746543088d1bc77ee1ceb4f8b
David Huggins-Daines <dhdaines@gmail.com>
2022-09-06 23:16:21 -0400

    feat: support the python-pocketsphinx API, almost
    AudioFile and LiveSpeech not quite working yet.  Also we no longer
    do stupid things like default to "goforward.raw" as the input

--- cython/pocketsphinx5/__init__.py
+++ cython/pocketsphinx5/__init__.py
@@ -1,8 +1,41 @@
 """Main module for the PocketSphinx speech recognizer.
 """
 
+# Copyright (c) 1999-2016 Carnegie Mellon University. All rights
+# reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#
+# This work was supported in part by funding from the Defense Advanced
+# Research Projects Agency and the National Science Foundation of the
+# United States of America, and the CMU Sphinx Speech Consortium.
+#
+# THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND
+# ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+# NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import collections
 import os
+import signal
+from contextlib import contextmanager
 
 from . import _pocketsphinx
 
@@ -20,8 +53,7 @@ from ._pocketsphinx import Endpointer  # noqa: F401
 from ._pocketsphinx import set_loglevel  # noqa: F401
 from .segmenter import Segmenter  # noqa: F401
 
-Arg = collections.namedtuple("Arg", ["name", "default", "doc",
-                                     "type", "required"])
+Arg = collections.namedtuple("Arg", ["name", "default", "doc", "type", "required"])
 Arg.__doc__ = "Description of a configuration parameter."
 Arg.name.__doc__ = "Parameter name (without leading dash)."
 Arg.default.__doc__ = "Default value of parameter."
@@ -46,3 +78,143 @@ def get_model_path(subpath=None):
         return os.path.join(model_path, subpath)
     else:
         return model_path
+
+
+class Pocketsphinx(Decoder):
+    def __init__(self, **kwargs):
+        if kwargs.get("dic") is not None and kwargs.get("dict") is None:
+            kwargs["dict"] = kwargs.pop("dic")
+        if kwargs.pop("verbose", False) is True:
+            kwargs["loglevel"] = "INFO"
+
+        super(Pocketsphinx, self).__init__(**kwargs)
+
+    def __str__(self):
+        return self.hypothesis()
+
+    @contextmanager
+    def start_utterance(self):
+        self.start_utt()
+        yield
+        self.end_utt()
+
+    @contextmanager
+    def end_utterance(self):
+        self.end_utt()
+        yield
+        self.start_utt()
+
+    def decode(self, audio_file, buffer_size=2048, no_search=False, full_utt=False):
+        buf = bytearray(buffer_size)
+
+        with open(audio_file, "rb") as f:
+            with self.start_utterance():
+                while f.readinto(buf):
+                    self.process_raw(buf, no_search, full_utt)
+        return self
+
+    def segments(self, detailed=False):
+        if detailed:
+            lmath = self.get_logmath()
+            return [(s.word, lmath.log(s.prob),
+                     s.start_frame, s.end_frame) for s in self.seg()]
+        else:
+            return [s.word for s in self.seg()]
+
+    def hypothesis(self):
+        hyp = self.hyp()
+        if hyp:
+            return hyp.hypstr
+        else:
+            return ""
+
+    def probability(self):
+        hyp = self.hyp()
+        if hyp:
+            return self.get_logmath().log(hyp.prob)
+
+    def score(self):
+        hyp = self.hyp()
+        if hyp:
+            return self.get_logmath().log(hyp.best_score)
+
+    def best(self, count=10):
+        lmath = self.get_logmath()
+        return [(h.hypstr, lmath.log(h.score)) for h, i in zip(self.nbest(), range(count))]
+
+    def confidence(self):
+        hyp = self.hyp()
+        if hyp:
+            return hyp.prob
+
+
+class AudioFile(Pocketsphinx):
+    def __init__(self, **kwargs):
+        signal.signal(signal.SIGINT, self.stop)
+
+        self.audio_file = kwargs.pop("audio_file", None)
+        self.buffer_size = kwargs.pop("buffer_size", 2048)
+        self.no_search = kwargs.pop("no_search", False)
+        self.full_utt = kwargs.pop("full_utt", False)
+
+        self.keyphrase = kwargs.get("keyphrase")
+
+        self.in_speech = False
+        self.buf = bytearray(self.buffer_size)
+
+        super(AudioFile, self).__init__(**kwargs)
+
+        self.f = open(self.audio_file, "rb")
+
+    def __iter__(self):
+        with self.f:
+            with self.start_utterance():
+                while self.f.readinto(self.buf):
+                    self.process_raw(self.buf, self.no_search, self.full_utt)
+                    if self.keyphrase and self.hyp():
+                        with self.end_utterance():
+                            yield self
+                    elif self.in_speech != self.get_in_speech():
+                        self.in_speech = self.get_in_speech()
+                        if not self.in_speech and self.hyp():
+                            with self.end_utterance():
+                                yield self
+
+    def stop(self, *args, **kwargs):
+        raise StopIteration
+
+
+class LiveSpeech(Pocketsphinx):
+    def __init__(self, **kwargs):
+        signal.signal(signal.SIGINT, self.stop)
+
+        self.audio_device = kwargs.pop("audio_device", None)
+        self.sampling_rate = kwargs.pop("sampling_rate", 16000)
+        self.buffer_size = kwargs.pop("buffer_size", 2048)
+        self.no_search = kwargs.pop("no_search", False)
+        self.full_utt = kwargs.pop("full_utt", False)
+
+        self.keyphrase = kwargs.get("keyphrase")
+
+        self.in_speech = False
+        self.buf = bytearray(self.buffer_size)
+        self.ad = Ad(self.audio_device, self.sampling_rate)
+
+        super(LiveSpeech, self).__init__(**kwargs)
+
+    def __iter__(self):
+        with self.ad:
+            with self.start_utterance():
+                while self.ad.readinto(self.buf) >= 0:
+                    self.process_raw(self.buf, self.no_search, self.full_utt)
+                    if self.keyphrase and self.hyp():
+                        with self.end_utterance():
+                            yield self
+                    elif self.in_speech != self.get_in_speech():
+                        self.in_speech = self.get_in_speech()
+                        if not self.in_speech and self.hyp():
+                            with self.end_utterance():
+                                yield self
+
+    def stop(self, *args, **kwargs):
+        raise StopIteration
---
+++ cython/test/pypocketsphinx_test.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+
+"""Test the various use cases for the old abandoned
+python-pocketsphinx module and its simple classes.  We don't support
+the truly useless parts of the API like defaulting to "goforward.raw"
+as the input, and some results have changed, other than that it should
+be compatible.
+"""
+
+import os
+from pocketsphinx5 import Pocketsphinx, AudioFile, NGramModel, Jsgf
+from unittest import TestCase, main
+
+MODELDIR = os.path.join(os.path.dirname(__file__), "../../model")
+DATADIR = os.path.join(os.path.dirname(__file__), "../../test/data")
+
+
+class TestAudioFile(TestCase):
+    def test_audiofile_raw(self):
+        hypothesis = ""
+        for phrase in AudioFile(audio_file=os.path.join(DATADIR, "goforward.raw")):
+            hypothesis = str(phrase)
+        self.assertEqual(hypothesis, "go forward ten meters")
+
+
+class TestRawDecoder(TestCase):
+    def setUp(self):
+        self.ps = Pocketsphinx(
+            hmm=os.path.join(MODELDIR, "en-us/en-us"),
+            lm=os.path.join(MODELDIR, "en-us/en-us.lm.bin"),
+            dict=os.path.join(MODELDIR, "en-us/cmudict-en-us.dict"),
+        )
+        self.ps.decode(os.path.join(DATADIR, "goforward.raw"))
+
+    def test_raw_decoder_lookup_word(self):
+        self.assertEqual(self.ps.lookup_word("hello"), "HH AH L OW")
+        self.assertEqual(self.ps.lookup_word("abcdf"), None)
+
+    def test_raw_decoder_hypothesis(self):
+        self.assertEqual(self.ps.hypothesis(), "go forward ten years")
+        self.assertEqual(self.ps.score(), -8240)
+        self.assertAlmostEqual(self.ps.confidence(), 0.05714680720514542, 3)
+
+    def test_raw_decoder_segments(self):
+        self.assertEqual(
+            self.ps.segments(),
+            ["<s>", "go", "forward", "ten", "years", "</s>"],
+        )
+
+    def test_raw_decoder_best_hypothesis(self):
+        self.assertEqual(
+            self.ps.best(),
+            [
+                ("go forward ten years", -28492),
+                ("go forward ten meters", -28543),
+                ("go for word ten meters", -29077),
+                ("go forward ten liters", -29085),
+                ("go forward ten leaders", -29099),
+                ("go forward can meters", -29175),
+                ("go for word ten years", -29217),
+                ("go for work ten meters", -29254),
+                ("go forward ten readers", -29255),
+                ("go forward can leaders", -29265),
+            ],
+        )
+
+
+class TestCepDecoder(TestCase):
+    def test_cep_decoder_hypothesis(self):
+        ps = Pocketsphinx(
+            hmm=os.path.join(MODELDIR, "en-us/en-us"),
+            lm=os.path.join(MODELDIR, "en-us/en-us.lm.bin"),
+            dict=os.path.join(MODELDIR, "en-us/cmudict-en-us.dict"),
+        )
+        with open(os.path.join(DATADIR, "goforward.mfc"), "rb") as f:
+            with ps.start_utterance():
+                f.read(4)
+                buf = f.read(13780)
+                ps.process_cep(buf, False, True)
+        self.assertEqual(ps.hypothesis(), "go forward ten meters")
+        self.assertEqual(ps.score(), -7095)
+        self.assertEqual(ps.probability(), -32715)
+
+
+class TestJsgf(TestCase):
+    def test_jsgf(self):
+        ps = Pocketsphinx(
+            hmm=os.path.join(MODELDIR, "en-us/en-us"),
+            lm=os.path.join(DATADIR, "turtle.lm.bin"),
+            dic=os.path.join(DATADIR, "turtle.dic"),
+        )
+
+        # Decoding with 'turtle' language model
+        ps.decode(os.path.join(DATADIR, "goforward.raw"))
+        self.assertEqual(ps.hypothesis(), "go forward ten meters")
+
+        # Switch to JSGF grammar
+        jsgf = Jsgf(os.path.join(DATADIR, "goforward.gram"))
+        rule = jsgf.get_rule("goforward.move2")
+        fsg = jsgf.build_fsg(rule, ps.get_logmath(), 7.5)
+        ps.set_fsg("goforward", fsg)
+        ps.set_search("goforward")
+
+        # Decoding with 'goforward' grammar
+        ps.decode(os.path.join(DATADIR, "goforward.raw"))
+        self.assertEqual(ps.hypothesis(), "go forward ten meters")
+
+
+class TestKws(TestCase):
+    def test_kws(self):
+        segments = []
+        for phrase in AudioFile(audio_file=os.path.join(DATADIR, "goforward.raw"),
+                                keyphrase="forward", kws_threshold=1e20):
+            segments = phrase.segments(detailed=True)
+        self.assertEqual(segments, [("forward", -676, 63, 121)])
+
+
+class TestLm(TestCase):
+    def test_lm(self):
+        ps = Pocketsphinx(
+            hmm=os.path.join(MODELDIR, "en-us/en-us"),
+            lm=os.path.join(MODELDIR, "en-us/en-us.lm.bin"),
+            dic=os.path.join(DATADIR, "defective.dic"),
+        )
+
+        # Decoding with 'defective' dictionary
+        ps.decode(os.path.join(DATADIR, "goforward.raw"))
+        self.assertEqual(ps.hypothesis(), "")
+
+        # Switch to 'turtle' language model
+        turtle_lm = os.path.join(DATADIR, "turtle.lm.bin")
+        lm = NGramModel(ps.get_config(), ps.get_logmath(), turtle_lm)
+        ps.set_lm("turtle", lm)
+        ps.set_search("turtle")
+
+        # Decoding with 'turtle' language model
+        ps.decode(os.path.join(DATADIR, "goforward.raw"))
+        self.assertEqual(ps.hypothesis(), "")
+
+        # The word 'meters' isn't in the loaded dictionary
+        # Let's add it manually
+        ps.add_word("foobie", "F UW B IY", False)
+        ps.add_word("meters", "M IY T ER Z", True)
+
+        # Decoding with 'turtle' language model
+        ps.decode(os.path.join(DATADIR, "goforward.raw"))
+        self.assertEqual(ps.hypothesis(), "foobie meters meters")
+
+
+class TestPhoneme(TestCase):
+    def setUp(self):
+        self.ps = Pocketsphinx(
+            allphone=os.path.join(MODELDIR, "en-us/en-us-phone.lm.bin"),
+            lw=2.0,
+            pip=0.3,
+            beam=1e-200,
+            pbeam=1e-20,
+        )
+        self.ps.decode(os.path.join(DATADIR, "goforward.raw"))
+
+    def test_phoneme_hypothesis(self):
+        self.assertEqual(
+            self.ps.hypothesis(), "SIL G OW F AO R D T AE N NG IY ZH ER S SIL"
+        )
+
+    def test_phoneme_best_phonemes(self):
+        self.assertEqual(
+            self.ps.segments(),
+            [
+                "SIL",
+                "G",
+                "OW",
+                "F",
+                "AO",
+                "R",
+                "D",
+                "T",
+                "AE",
+                "N",
+                "NG",
+                "IY",
+                "ZH",
+                "ER",
+                "S",
+                "SIL",
+            ],
+        )
+
+
+if __name__ == "__main__":
+    main(verbosity=2)
