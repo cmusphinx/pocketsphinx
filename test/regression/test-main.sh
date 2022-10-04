0dafa01caea5836987f6cc8f48ba3257bd73a0f8
David Huggins-Daines <dhdaines@gmail.com>
2022-10-04 15:18:16 -0400

    test: test POCKETSPHINX_PATH

--- test/regression/test-main.sh
+++ test/regression/test-main.sh
@@ -6,12 +6,9 @@
 bn=`basename $0 .sh`
 
 echo "Test: $bn"
+export POCKETSPHINX_PATH=$model
 sox $data/librivox/*.wav $(run_program pocketsphinx soxflags) | \
-    run_program pocketsphinx \
-                -loglevel INFO \
-                -hmm $model/en-us/en-us \
-                -lm $model/en-us/en-us.lm.bin \
-                -dict $model/en-us/cmudict-en-us.dict - \
+    run_program pocketsphinx -loglevel INFO - \
                 2>$bn.log >$bn.json
 
 # Test whether it actually completed
