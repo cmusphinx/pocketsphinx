132780c1ad2314e6c1f09ea9ed1418394a33f24e
David Huggins-Daines <dhdaines@gmail.com>
2022-10-05 08:43:50 -0400

    build: bump version (yes to the final one)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, David Huggins-Daines'
 author = 'David Huggins-Daines'
 
 # The full version, including alpha/beta/rc tags
-release = '5.0.0rc5'
+release = '5.0.0'
 
 
 # -- General configuration ---------------------------------------------------
