35f2e5f266f48858b9d644509f9152351c445a4c
David HUggins-Daines <dhdaines@gmail.com>
2022-10-21 19:19:20 -0400

    fix: make this work properly...
    ... or as well as it is gonna work with my laptop and its crap audio

--- examples/live_win32.c
+++ examples/live_win32.c
@@ -23,6 +23,14 @@
 #include <pocketsphinx.h>
 #include <signal.h>
 
+static int global_done = 0;
+static void
+catch_sig(int signum)
+{
+    (void)signum;
+    global_done = 1;
+}
+
 #define CHECK(expr)                                             \
     do {                                                        \
         int err;                                                \
@@ -39,20 +47,19 @@ int main(int argc, char *argv[])
     ps_decoder_t *decoder;
     ps_config_t *config;
     ps_endpointer_t *ep;
-    short *frame;
     size_t frame_size;
-    int err;
     HWAVEIN wavein;
     WAVEFORMATEX wavefmt;
     HANDLE event;
-    WAVEHDR hdrs[32];
+    /* A large but somewhat arbitrary number of buffers. */
+#define NBUF 100 /* 100 * 0.03 = 3 seconds */
+    WAVEHDR hdrs[NBUF];
     int i;
-    size_t bufsize;
 
+    (void)argc; (void)argv;
     /* Initialize decoder and endpointer */
     config = ps_config_init(NULL);
     ps_default_search_args(config);
-    ps_config_set_str(config, "loglevel", "INFO");
     if ((decoder = ps_init(config)) == NULL)
         E_FATAL("PocketSphinx decoder init failed\n");
     if ((ep = ps_endpointer_init(0, 0.0, 0,
@@ -61,8 +68,6 @@ int main(int argc, char *argv[])
         E_FATAL("PocketSphinx endpointer init failed\n");
     /* Frame size in samples (not bytes) */
     frame_size = ps_endpointer_frame_size(ep);
-    if ((frame = malloc(frame_size * 2)) == NULL)
-        E_FATAL_SYSTEM("Failed to allocate frame");
     /* Tell Windows what format we want (NOTE: may not be available...) */
     wavefmt.wFormatTag = WAVE_FORMAT_PCM;
     wavefmt.nChannels = 1;
@@ -72,16 +77,15 @@ int main(int argc, char *argv[])
     wavefmt.nAvgBytesPerSec = wavefmt.nSamplesPerSec * wavefmt.nBlockAlign;
     wavefmt.cbSize = 0;
     /* Create an event to tell us when a new buffer is ready. */
-    event = CreateEventA(NULL, TRUE, FALSE, "buffer_ready");
+    event = CreateEvent(NULL, TRUE, FALSE, "buffer_ready");
     /* Open the recording device. */
     CHECK(waveInOpen(&wavein, WAVE_MAPPER, &wavefmt,
                      (DWORD_PTR)event, 0, CALLBACK_EVENT));
-    /* Create a good but arbitrary number of buffers. */
+    /* Create buffers. */
     memset(hdrs, 0, sizeof(hdrs));
-    bufsize = frame_size * 2;
-    for (i = 0; i < 32; ++i) {
-        hdrs[i].lpData = malloc(bufsize);
-        hdrs[i].dwBufferLength = (DWORD)bufsize;
+    for (i = 0; i < NBUF; ++i) {
+        hdrs[i].lpData = malloc(frame_size * 2);
+        hdrs[i].dwBufferLength = (DWORD)frame_size * 2;
         CHECK(waveInPrepareHeader(wavein, &hdrs[i], sizeof(hdrs[i])));
         CHECK(waveInAddBuffer(wavein, &hdrs[i], sizeof(hdrs[i])));
     }
@@ -92,34 +96,41 @@ int main(int argc, char *argv[])
         E_FATAL_SYSTEM("Failed to set SIGINT handler");
     while (!global_done) {
         const int16 *speech;
-        int prev_in_speech = ps_endpointer_in_speech(ep);
         WaitForSingleObject(event, INFINITE);
         /* Get as many buffers as we can. */
         while (hdrs[i].dwFlags & WHDR_DONE) {
+            int prev_in_speech = ps_endpointer_in_speech(ep);
+            int16 *frame = (int16 *)hdrs[i].lpData;
+            /* Process them one by one. */
+            speech = ps_endpointer_process(ep, frame);
             CHECK(waveInUnprepareHeader(wavein, &hdrs[i], sizeof(hdrs[i])));
             CHECK(waveInPrepareHeader(wavein, &hdrs[i], sizeof(hdrs[i])));
             CHECK(waveInAddBuffer(wavein, &hdrs[i], sizeof(hdrs[i])));
-            if (++i == 32)
+            if (++i == NBUF)
                 i = 0;
-            /* Process them one by one. */
-            speech = ps_endpointer_process(ep, frame);
             if (speech != NULL) {
                 const char *hyp;
                 if (!prev_in_speech) {
                     fprintf(stderr, "Speech start at %.2f\n",
                             ps_endpointer_speech_start(ep));
+		    fflush(stderr); /* For broken MSYS2 terminal */
                     ps_start_utt(decoder);
                 }
                 if (ps_process_raw(decoder, speech, frame_size, FALSE, FALSE) < 0)
                     E_FATAL("ps_process_raw() failed\n");
-                if ((hyp = ps_get_hyp(decoder, NULL)) != NULL)
+                if ((hyp = ps_get_hyp(decoder, NULL)) != NULL) {
                     fprintf(stderr, "PARTIAL RESULT: %s\n", hyp);
+		    fflush(stderr);
+		}
                 if (!ps_endpointer_in_speech(ep)) {
                     fprintf(stderr, "Speech end at %.2f\n",
                             ps_endpointer_speech_end(ep));
+		    fflush(stderr);
                     ps_end_utt(decoder);
-                    if ((hyp = ps_get_hyp(decoder, NULL)) != NULL)
+                    if ((hyp = ps_get_hyp(decoder, NULL)) != NULL) {
                         printf("%s\n", hyp);
+			fflush(stdout);
+		    }
                 }
             }
         }
@@ -129,12 +140,16 @@ int main(int argc, char *argv[])
     /* Stop recording, cancel all buffers, and free them. */
     CHECK(waveInStop(wavein));
     CHECK(waveInReset(wavein));
-    for (i = 0; i < 32; ++i) {
+    for (i = 0; i < NBUF; ++i) {
         if (hdrs[i].dwFlags & WHDR_PREPARED)
             CHECK(waveInUnprepareHeader(wavein, &hdrs[i],
                                         sizeof(hdrs[i])));
         free(hdrs[i].lpData);
     }
+    CloseHandle(event);
+    ps_endpointer_free(ep);
+    ps_free(decoder);
+    ps_config_free(config);
 
     return 0;
 }
