abebda9db290cb9125126d39db3993c1724fa98a
David Huggins-Daines <dhd@ecolingui.ca>
2023-03-05 07:26:48 -0500

    chore: bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, David Huggins-Daines'
 author = 'David Huggins-Daines'
 
 # The full version, including alpha/beta/rc tags
-release = '5.0.0'
+release = '5.0.1'
 
 
 # -- General configuration ---------------------------------------------------
