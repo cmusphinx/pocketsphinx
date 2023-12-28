96e06baf781d59c17ecf7fe5a6a1cdb3031ebfd5
David Huggins-Daines <dhd@ecolingui.ca>
2023-12-28 12:33:09 -0500

    fix: switch to scikit-build-core for python 3.12 and editable

--- setup.py
+++
@@ -1,9 +0,0 @@
-from skbuild import setup
-from setuptools import find_packages
-
-setup(
-    packages=find_packages('cython', exclude=["test"]),
-    package_dir={"": "cython"},
-    cmake_languages=["C"],
-    install_requires=["sounddevice"],
-)
