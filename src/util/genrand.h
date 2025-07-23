e03e990fb6ed9d48871400e7251f96c6597988b6
Kevin Lenzo <lenzo@duolingo.com>
2025-07-23 08:49:03 -0400

    feat: Make random number generator thread-safe using TLS
    Updates the Mersenne Twister RNG implementation to use thread-local
    storage for its state variables (mt[] array and mti index). This
    eliminates race conditions when multiple threads use the RNG
    concurrently.
    
    Changes:
    - Make mt[N] array thread-local using PS_THREAD_LOCAL
    - Make mti index thread-local using PS_THREAD_LOCAL
    - Add thread safety documentation to header
    - Maintain backward compatibility - API unchanged
    
    When PS_USE_THREAD_LOCAL_RNG is defined (set via CMake), each thread
    gets its own RNG state, preventing data races and ensuring
    deterministic behavior in multi-threaded applications.

--- src/util/genrand.h
+++ src/util/genrand.h
@@ -1,13 +1,13 @@
 /* -*- c-basic-offset: 4; indent-tabs-mode: nil -*- */
-/* 
+/*
    A C-program for MT19937, with initialization improved 2002/1/26.
    Coded by Takuji Nishimura and Makoto Matsumoto.
 
-   Before using, initialize the state by using init_genrand(seed)  
+   Before using, initialize the state by using init_genrand(seed)
    or init_by_array(init_key, key_length).
 
    Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
-   All rights reserved.                          
+   All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
@@ -20,8 +20,8 @@
 `        notice, this list of conditions and the following disclaimer in the
         documentation and/or other materials provided with the distribution.
 
-     3. The names of its contributors may not be used to endorse or promote 
-        products derived from this software without specific prior written 
+     3. The names of its contributors may not be used to endorse or promote
+        products derived from this software without specific prior written
         permission.
 
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -51,27 +51,27 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
  *
- * This work was supported in part by funding from the Defense Advanced 
- * Research Projects Agency and the National Science Foundation of the 
+ * This work was supported in part by funding from the Defense Advanced
+ * Research Projects Agency and the National Science Foundation of the
  * United States of America, and the CMU Sphinx Speech Consortium.
  *
- * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
- * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
  * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * ====================================================================
@@ -79,8 +79,8 @@
  */
 
 /*
- * randgen.c   : a portable random generator 
- * 
+ * randgen.c   : a portable random generator
+ *
  *
  * **********************************************
  * CMU ARPA Speech Project
@@ -88,7 +88,7 @@
  * Copyright (c) 1999 Carnegie Mellon University.
  * ALL RIGHTS RESERVED.
  * **********************************************
- * 
+ *
  * HISTORY
  * $Log: genrand.h,v $
  * Revision 1.3  2005/06/22 03:01:50  arthchan2003
@@ -97,18 +97,18 @@
  * Revision 1.3  2005/03/30 01:22:48  archan
  * Fixed mistakes in last updates. Add
  *
- * 
+ *
  * 18-Nov-04 ARCHAN (archan@cs.cmu.edu) at Carnegie Mellon University
  *              First incorporated from the Mersenne Twister Random
  *              Number Generator package. It was chosen because it is
  *              in BSD-license and its performance is quite
  *              reasonable. Of course if you look at the inventors's
  *              page.  This random generator can actually gives
- *              19937-bits period.  This is already far from we need. 
- *              This will possibly good enough for the next 10 years. 
+ *              19937-bits period.  This is already far from we need.
+ *              This will possibly good enough for the next 10 years.
  *
  *              I also downgrade the code a little bit to avoid Sphinx's
- *              developers misused it. 
+ *              developers misused it.
  */
 
 #ifndef _LIBUTIL_GENRAND_H_
@@ -118,13 +118,23 @@
 #include <stdio.h>
 
 /** \file genrand.h
- *\brief High performance prortable random generator created by Takuji
- *Nishimura and Makoto Matsumoto.  
- * 
+ *\brief High performance portable random generator created by Takuji
+ *Nishimura and Makoto Matsumoto.
+ *
  * A high performance which applied Mersene twister primes to generate
- * random number. If probably seeded, the random generator can achieve 
- * 19937-bits period.  For technical detail.  Please take a look at 
- * (FIXME! Need to search for the web site.) http://www.
+ * random number. If properly seeded, the random generator can achieve
+ * 19937-bits period.  For technical detail.  Please take a look at
+ * http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+ *
+ * Thread Safety:
+ * When PS_THREAD_LOCAL_RNG is defined (via CMake option), the random
+ * number generator uses thread-local storage, making it thread-safe.
+ * Each thread maintains its own independent RNG state. This is enabled
+ * by default on platforms with thread-local storage support.
+ *
+ * Without PS_THREAD_LOCAL_RNG, the RNG uses global state and is NOT
+ * thread-safe. Multiple threads accessing the RNG concurrently will
+ * result in race conditions and unpredictable sequences.
  */
 #ifdef __cplusplus
 extern "C" {
@@ -144,17 +154,17 @@ extern "C" {
 #define s3_rand_res53()  genrand_res53()
 
 /**
- *Initialize the seed of the random generator. 
+ *Initialize the seed of the random generator.
  */
 void genrand_seed(unsigned long s);
 
 /**
- *generates a random number on [0,0x7fffffff]-interval 
+ *generates a random number on [0,0x7fffffff]-interval
  */
 long genrand_int31(void);
 
 /**
- *generates a random number on (0,1)-real-interval 
+ *generates a random number on (0,1)-real-interval
  */
 double genrand_real3(void);
 
