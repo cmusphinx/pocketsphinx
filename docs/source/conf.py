d7c0c8e91906bdebc0012b418ed416a1266824f6
David Huggins-Daines <dhd@ecolingui.ca>
2026-06-06 12:38:29 -0400

    chore: prepare for 5.1.1 release by bumping version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, David Huggins-Daines'
 author = 'David Huggins-Daines'
 
 # The full version, including alpha/beta/rc tags
-release = '5.1.0'
+release = '5.1.1rc0'
 
 
 # -- General configuration ---------------------------------------------------
